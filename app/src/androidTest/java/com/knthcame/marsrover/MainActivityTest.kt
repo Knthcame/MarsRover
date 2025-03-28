0128f27af15e02be18372a93a40f6ebd065f8119
Kenneth Cartanyà Mendoza <knthcame@gmail.com>
2025-03-28 02:32:52 +0100

    Checkout MainActivityTest from main. Updated README

---
+++ app/src/androidTest/java/com/knthcame/marsrover/MainActivityTest.kt
@@ -0,0 +1,35 @@
+package com.knthcame.marsrover
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @get:Rule
+    val composeActivityRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun testPackageName() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.knthcame.marsrover", appContext.packageName)
+    }
+
+    @Test
+    fun testLaunch() {
+        composeActivityRule.onNodeWithTag("homeTopBarTitle", useUnmergedTree = true)
+            .assertIsDisplayed()
+    }
+}
