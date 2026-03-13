8326c2b9720592bc20fa15edd745f2e69e699dbc
Kenneth Cartanyà Mendoza <39163040+Knthcame@users.noreply.github.com>
2026-03-13 14:48:58 +0100

    Update MovementsScreenTest.kt

--- app/src/androidTest/java/com/knthcame/marsrover/ui/movements/MovementsScreenTest.kt
+++ app/src/androidTest/java/com/knthcame/marsrover/ui/movements/MovementsScreenTest.kt
@@ -5,18 +5,14 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
-import androidx.lifecycle.SavedStateHandle
 import com.knthcame.marsrover.HiltTestActivity
-import com.knthcame.marsrover.data.calculation.RoverPositionCalculator
 import com.knthcame.marsrover.data.control.models.CardinalDirection
-import com.knthcame.marsrover.data.control.repositories.RoverRepository
-import com.knthcame.marsrover.testViewModelScope
+import com.knthcame.marsrover.ui.navigation.Movements
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import javax.inject.Inject
 
 @HiltAndroidTest
 class MovementsScreenTest {
@@ -26,12 +22,6 @@ class MovementsScreenTest {
     @get:Rule(order = 1)
     val composeRule = createAndroidComposeRule<HiltTestActivity>()
 
-    @Inject
-    lateinit var roverRepository: RoverRepository
-
-    @Inject
-    lateinit var roverPositionCalculator: RoverPositionCalculator
-
     @Before
     fun before() {
         hiltRule.inject()
@@ -39,26 +29,18 @@ class MovementsScreenTest {
 
     @Test
     fun movementsTextField_displaysMovementsSequence_accordingToUserInput() {
-        val savedStateHandle = SavedStateHandle(
-            mapOf(
-                "plateauHeight" to 5,
-                "plateauWidth" to 5,
-                "initialPositionX" to 1,
-                "initialPositionY" to 2,
-                "initialDirection" to CardinalDirection.North,
-            )
-        )
-        val vm = MovementsViewModel(
-            savedStateHandle = savedStateHandle,
-            roverRepository = roverRepository,
-            roverPositionCalculator = roverPositionCalculator,
-            viewModeScope = testViewModelScope,
+        val movements = Movements(
+            plateauHeight = 5,
+            plateauWidth = 5,
+            initialPositionX = 1,
+            initialPositionY = 2,
+            initialDirection = CardinalDirection.North,
         )
 
         composeRule.setContent {
             MovementsScreenRoute(
+                movements = movements,
                 onNavigateBack = { },
-                viewModel = vm,
             )
         }
 
