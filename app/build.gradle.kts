7a153484a5e76afaa80c6959c13990209e761037
Kenneth Cartanyà Mendoza <39163040+Knthcame@users.noreply.github.com>
2026-06-05 09:20:50 +0200

    Migrate android sdk versions to toml file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,12 +22,12 @@ kotlin {
 
 configure<ApplicationExtension> {
     namespace = "com.knthcame.marsrover"
-    compileSdk = 37
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.knthcame.marsrover"
-        minSdk = 24
-        targetSdk = 37
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
