b3b0957b8bf86583d1b0c117b933890a590f758a
Kenneth Cartanyà Mendoza <39163040+Knthcame@users.noreply.github.com>
2026-07-21 17:59:13 +0200

    Clean up duplicated gradle dependency declarations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,17 +78,12 @@ dependencies {
     testImplementation(libs.mockk.android)
     testImplementation(libs.mockk.agent)
     testImplementation(libs.app.cash.turbine)
-    testImplementation(libs.hilt.android.testing)
-    kspTest(libs.hilt.compiler)
 
-    androidTestImplementation(libs.kotlin.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    androidTestImplementation(libs.app.cash.turbine)
     androidTestImplementation(libs.hilt.android.testing)
-    kspAndroidTest(libs.hilt.compiler)
 
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
